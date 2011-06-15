0b1b8c372bcf0eeb757e5b6b329ac96867291e0c
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-06-14 22:24:31 -0400

    added tool to help setup server apps faster

---
+++ server_admin/install_basic.sh
@@ -0,0 +1,4 @@
+apt-get update
+apt-get upgrade -Y
+apt-get install apache2 php5 php5-dev git php-pear curl php5-curl php5-xmlrpc libcurl3-openssl-dev mcrypt php5-mcrypt libpcre3-dev openjdk-6-jdk -Y
+aptitude install libboost-dev python-dev autoconf automake pkg-config make libtool flex bison build-essential
